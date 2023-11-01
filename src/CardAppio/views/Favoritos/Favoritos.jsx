c56a1702138115b0c6a2a7c450b03e11e95ab5eb
Claudio Barroso <claudiolcbarroso@gmail.com>
2023-10-31 22:08:59 -0300

    Inicio construcao page favoritos

--- src/CardAppio/views/Favoritos/Favoritos.jsx
+++ src/CardAppio/views/Favoritos/Favoritos.jsx
@@ -1,12 +1,34 @@
-import { Text } from "react-native-paper"
+import { Appbar, Text, Button } from 'react-native-paper';
+import { FlatList, Image, ScrollView, StyleSheet, View, } from 'react-native';
+
+import Header from '../../components/Header/Header'
+import COLORS from '../../constants/colors';
+
 
 const Favoritos = () => {
 
     return (
-        <>
-            <Text> Favoritos </Text>
-        </>
+        <ScrollView style={{ backgroundColor: COLORS.black }}>
+            <Header>
+                <Appbar.Action icon="menu" onPress={() => { }} />
+                <Appbar.Content titleStyle={{ fontSize: 24, fontWeight: 'bold' }} title="Favoritos" onPress={() => { }} />
+                <Appbar.Action icon="account" onPress={() => { }} />
+            </Header>
+            <View style={styles.body}>
+
+            </View>
+
+        </ ScrollView>
     )
 }
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        backgroundColor: COLORS.black,
+    },
+
+  
+})
+
 
 export default Favoritos;
