ad159cfd6a4f04b82db4148e229fb8316723b7e3
CostaGabriel022 <gabrielcostabispo23@gmail.com>
2023-11-09 12:18:23 -0300

    commit inicial tela-menu

--- src/CardAppio/views/Menu/Menu.jsx
+++ src/CardAppio/views/Menu/Menu.jsx
@@ -1,12 +1,72 @@
-import { Text } from "react-native-paper"
+import { View, FlatList, Text, StyleSheet, Image } from 'react-native';
 
-const Menu = () => {
+import Header from '../../components/Header/Header'
+import Container from '../../components/Container/Containers'
+import Body from '../../components/Body/Body'
 
-    return (
-        <>
-            <Text> Menu </Text>
-        </>
-    )
-}
+const menuItems = [
+  {
+    id: '1',
+    name: 'Item 1',
+    description: 'Uma breve descrição do Item 1',
+    price: 'R$ 10.99',
+   // image:
+  },
+];
 
-export default Menu;
+const MenuList = () => {
+  return (
+    <Container>
+    
+      <Header />
+      <Body>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <Image source={item.image} style={styles.image} />
+              <View style={styles.itemDetails}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.price}>{item.price}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </Body>
+
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    padding: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  itemDetails: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 14,
+  },
+  price: {
+    fontSize: 16,
+    marginTop: 5,
+    color: 'green',
+  },
+});
+
+export default MenuList;
